2a0426e29656c58b6a0b3feee5ae9b77374303af
silvamfpedro <silva.mfpedro@gmail.com>
2020-03-16 14:58:03 +0000

    Added changes in image streaming files

--- imagezmq-streaming/client.py
+++ imagezmq-streaming/client.py
@@ -16,17 +16,24 @@ import time
 # Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True, help="IP address of the server to which the client will connect")
+ap.add_argument("-r", "--picamera", type=int, default=-1, help="whether or not the Raspberry Pi camera should be used")
 args = vars(ap.parse_args())
 
 # Initialize the ImageSender object with the socket address of the server
-sender = ImageSender(connect_to="tcp://{}:5555".format(args["server_ip"]))
+sender = ImageSender()
 
 # Get the host name, initialize the video stream and allow the camera sensor to warmup
 rpi_name = socket.gethostname()
-vs = VideoStream(usePiCamera=True).start()
+print("[INFO] Camera sensor warming up...")
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 time.sleep(2.0)
 
+count = 0
 while True:
+	# Increment the counter and print its value to the console
+	count = count + 1
+	print("Sending: {}".format(count))
 	# Read the frame from the camera and send it to the server
 	frame = vs.read()
 	sender.send_image(rpi_name, frame)
+	time.sleep(1)
--- imagezmq-streaming/server.py
+++ imagezmq-streaming/server.py
@@ -1,8 +1,7 @@
 # -----------------------------
 #   USAGE
 # -----------------------------
-# python server.py --prototxt MobileNetSSD_deploy.prototxt --model MobileNetSSD_deploy.caffemodel
-# --montageW 2 --montageH 2
+# python server.py --prototxt MobileNetSSD_deploy.prototxt --model MobileNetSSD_deploy.caffemodel --montageW 2 --montageH 2
 
 # -----------------------------
 #   IMPORTS
