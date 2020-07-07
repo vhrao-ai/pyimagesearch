3e5c922b5f905078322d2283d704ef8875f043e0
silvamfpedro <silva.mfpedro@gmail.com>
2020-07-07 16:19:27 +0100

    Added changes in region_proposal_detection.py

--- region-proposal-object-detection/region_proposal_detection.py
+++ region-proposal-object-detection/region_proposal_detection.py
@@ -8,7 +8,7 @@
 # -----------------------------
 # Import the necessary packages
 from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.applications.resnet50 import preprocess_inputs
+from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.applications import imagenet_utils
 from tensorflow.keras.preprocessing.image import img_to_array
 from imutils.object_detection import non_max_suppression
